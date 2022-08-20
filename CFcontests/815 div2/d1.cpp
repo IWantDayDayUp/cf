a4e927bbeb56487efa15da36ec599a7f81c99fc1
IWantDayDayUP <1462959616@qq.com>
2022-08-20 10:54:57 +0800

    cf 815 D1

--- CFcontests/815 div2/d1.cpp
+++ CFcontests/815 div2/d1.cpp
@@ -6,7 +6,25 @@ void solve() {
     int n;
     cin >> n;
 
-    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> dp(n, 1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0 && (i - j) <= 400; j--)
+        {
+            if ((a[j] ^ i) < (a[i] ^ j))
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end()) << endl;
 }
 
 int main() {
